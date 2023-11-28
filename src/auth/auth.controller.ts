499431566fc1b514b3fcf15d2010c0f1cbd04776
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-11-28 05:14:56 +0200

    1- use class-validator on the dto to validation email and pass 2- add native nest validationPipe on the globalPipes middleware inside main.ts 3- filter all other fields that mentioned in dto out using whitelist

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,15 +4,15 @@ import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
-  @Post('Login')
-  login(@Body() dto: AuthDto) {
-    console.log({ dto });
-    return this.authService.login();
-  }
+   constructor(private authService: AuthService) {}
+   @Post('Login')
+   login(@Body() dto: AuthDto) {
+      console.log({ dto });
+      return this.authService.login();
+   }
 
-  @Post('Signup')
-  signup(req, res) {
-    return this.authService.signup();
-  }
+   @Post('Signup')
+   signup(req, res) {
+      return this.authService.signup();
+   }
 }
--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,5 +1,11 @@
-export interface AuthDto {
-  email: string;
-  password: string;
-  passwordConfirmation: string;
+import { IsEmail, IsNotEmpty, IsStrongPassword } from 'class-validator';
+
+export class AuthDto {
+   @IsEmail()
+   @IsNotEmpty()
+   email: string;
+
+   @IsStrongPassword()
+   @IsNotEmpty()
+   password: string;
 }
