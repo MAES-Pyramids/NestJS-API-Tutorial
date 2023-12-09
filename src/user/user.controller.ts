3dabba777a236489bf82b9fa7cf113ed7a53704f
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-12-10 01:44:50 +0200

    Substituted Passport's authGuard with a custom JwtGuard to avoid explicit typing of the JWT string keyword.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
 
 @Controller('user')
 export class UserController {
    @Get()
-   @UseGuards(AuthGuard('jwt'))
+   @UseGuards(JwtGuard)
    getUsers(@Req() req: Request) {
       return req.user;
    }
