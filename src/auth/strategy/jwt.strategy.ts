418306362de7ef0bc2e3f66e53d7538073b47b52
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-12-10 01:08:46 +0200

    Implemented a 'validate' function in the JWT strategy, accepting JWT payload as input, which enriches the Express 'req' object with the payload data. Additionally, the 'getUserById' functionality has been incorporated within this 'validate' function to return actual user from database into the req object

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,14 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-   constructor(config: ConfigService) {
+   constructor(
+      config: ConfigService,
+      private prisma: PrismaService,
+   ) {
       super({
          jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
          secretOrKey: config.get('JWT_Secret'),
       });
+   }
+
+   async validate(payload: any) {
+      const user = await this.prisma.user.findUnique({
+         where: {
+            id: payload.sub,
+         },
+      });
+      if (!user) {
+         throw new UnauthorizedException();
+      }
+      delete user.hashedPass;
+      return user;
    }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 
 @Controller('user')
 export class UserController {
    @Get()
    @UseGuards(AuthGuard('jwt'))
-   getUsers() {
-      return `it's me`;
+   getUsers(@Req() req: Request) {
+      return req.user;
    }
 }
