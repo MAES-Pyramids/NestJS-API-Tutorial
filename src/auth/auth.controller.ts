4dc514d3c65e9dadd253f681340c12a00c611b9b
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-12-10 02:17:50 +0200

    Include basic decorators to modify the status code of controller methods, along with additional custom decorators to access user objects from an Express request.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
    constructor(private authService: AuthService) {}
+   @HttpCode(201)
    @Post('Signup')
    async signup(@Body() dto: AuthDto) {
       return await this.authService.signup(dto);
    }
 
+   @HttpCode(HttpStatus.OK) // HttpStatus.OK => 200
    @Post('Login')
    login(@Body() dto: AuthDto) {
       return this.authService.login(dto);
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { Request } from 'express';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
+import { UserService } from './user.service';
 
+@UseGuards(JwtGuard)
 @Controller('user')
 export class UserController {
-   @Get()
-   @UseGuards(JwtGuard)
-   getUsers(@Req() req: Request) {
-      return req.user;
+   constructor(private userService: UserService) {}
+
+   @Get('me')
+   getMe(@GetUser() user: User) {
+      return user;
    }
 }
