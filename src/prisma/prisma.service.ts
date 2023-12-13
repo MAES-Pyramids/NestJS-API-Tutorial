7a5924bc6afda00093bc822aa8a0de6b6a278152
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-12-13 23:08:17 +0200

    Switched to dotenv-cli for dynamic schema.prisma database path; two env files maintain separate values for package.json scripts.

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -5,16 +5,10 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient {
    constructor(config: ConfigService) {
-      const NodeEnvironment: string = config.get('NODE_ENV');
-      const url: string =
-         NodeEnvironment == 'testing'
-            ? config.get('DATABASE_Testing_URL')
-            : config.get('DATABASE_URL');
-
       super({
          datasources: {
             db: {
-               url,
+               url: config.get('DATABASE_URL'),
             },
          },
       });
