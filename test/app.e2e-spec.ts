6b4f6be7df2bdfda5d9385db053c5d06e45d5957
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-12-14 03:53:03 +0200

    login jest testing impalemented inside Auth test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -72,5 +72,37 @@ describe('AppController (e2e)', () => {
                .expectStatus(201);
          });
       });
+
+      describe('Signin', () => {
+         it('should throw if email empty', () => {
+            return pactum
+               .spec()
+               .post('/auth/login')
+               .withBody({
+                  password: dto.password,
+               })
+               .expectStatus(400);
+         });
+         it('should throw if password empty', () => {
+            return pactum
+               .spec()
+               .post('/auth/login')
+               .withBody({
+                  email: dto.email,
+               })
+               .expectStatus(400);
+         });
+         it('should throw if no body provided', () => {
+            return pactum.spec().post('/auth/login').expectStatus(400);
+         });
+         it('should signin', () => {
+            return pactum
+               .spec()
+               .post('/auth/login')
+               .withBody(dto)
+               .expectStatus(200)
+               .stores('userAt', 'access_token');
+         });
+      });
    });
 });
