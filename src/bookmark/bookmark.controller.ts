bc408e7098cc54f960bb5e60300ea708a2007e8f
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-12-14 21:56:02 +0200

    Bookmarks services got impalemented

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -14,36 +14,49 @@ import {
 import { JwtGuard } from '../auth/guard';
 import { GetUser } from '../auth/decorator';
 import { CreateBookmarkDto, EditBookmarkDto } from './dto';
+import { BookmarkService } from './bookmark.service';
 
 @UseGuards(JwtGuard)
 @Controller('bookmark')
 export class BookmarkController {
+   constructor(private bookmarkService: BookmarkService) {}
+
    @Get()
-   getBookmarks(@GetUser('id') userId: number) {}
+   getBookmarks(@GetUser('id') userId: number) {
+      return this.bookmarkService.getBookmarks(userId);
+   }
 
    @Post()
    createBookmark(
       @GetUser('id') userId: number,
       @Body() dto: CreateBookmarkDto,
-   ) {}
+   ) {
+      return this.bookmarkService.createBookmark(userId, dto);
+   }
 
    @Get(':id')
    getBookmarkById(
       @GetUser('id') userId: number,
       @Param('id', ParseIntPipe) bookmarkId: number,
-   ) {}
+   ) {
+      return this.bookmarkService.getBookmarkById(userId, bookmarkId);
+   }
 
    @Patch(':id')
    editBookmark(
       @GetUser('id') userId: number,
       @Param('id', ParseIntPipe) bookmarkId: number,
       @Body() dto: EditBookmarkDto,
-   ) {}
+   ) {
+      return this.bookmarkService.editBookmark(userId, bookmarkId, dto);
+   }
 
    @HttpCode(HttpStatus.NO_CONTENT)
    @Delete(':id')
    deleteBookmark(
       @GetUser('id') userId: number,
       @Param('id', ParseIntPipe) bookmarkId: number,
-   ) {}
+   ) {
+      return this.bookmarkService.deleteBookmark(userId, bookmarkId);
+   }
 }
--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,4 +1,75 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { CreateBookmarkDto, EditBookmarkDto } from './dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class BookmarkService {}
+export class BookmarkService {
+   constructor(private prisma: PrismaService) {}
+
+   async getBookmarks(userId: number) {
+      const bookmarks = await this.prisma.bookmark.findMany({
+         where: {
+            userId,
+         },
+      });
+      return bookmarks;
+   }
+
+   async createBookmark(userId: number, dto: CreateBookmarkDto) {
+      const bookmark = await this.prisma.bookmark.create({
+         data: {
+            userId,
+            ...dto,
+         },
+      });
+      return bookmark;
+   }
+
+   async getBookmarkById(userId: number, bookmarkId: number) {
+      const bookmark = await this.prisma.bookmark.findFirst({
+         where: {
+            userId,
+            id: bookmarkId,
+         },
+      });
+      return bookmark;
+   }
+
+   async editBookmark(
+      userId: number,
+      bookmarkId: number,
+      dto: EditBookmarkDto,
+   ) {
+      const bookmark = await this.prisma.bookmark.findUnique({
+         where: {
+            id: bookmarkId,
+         },
+      });
+      if (!bookmark || bookmark.userId !== userId) {
+         throw new ForbiddenException('Access to resources denied');
+      }
+      const updatedBookmark = await this.prisma.bookmark.update({
+         where: { id: bookmarkId },
+         data: {
+            ...dto,
+         },
+      });
+      return updatedBookmark;
+   }
+
+   async deleteBookmark(userId: number, bookmarkId: number) {
+      const bookmark = await this.prisma.bookmark.findUnique({
+         where: {
+            id: bookmarkId,
+         },
+      });
+      if (!bookmark || bookmark.userId !== userId) {
+         throw new ForbiddenException('Access to resources denied');
+      }
+      await this.prisma.bookmark.delete({
+         where: {
+            id: bookmarkId,
+         },
+      });
+   }
+}
